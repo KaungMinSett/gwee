b7845500ba8dc11b0a5f4d68d7b12605597ad8c3
Your Name <you@example.com>
2022-07-17 00:27:40 +0630

    login process finished

--- review_post/routes.py
+++ review_post/routes.py
@@ -29,23 +29,20 @@ def login():
     For POSTS, login the current user by processing the form. """
     # print (db)
     form = LoginForm()
-    print('----------------------ssd-------------')
-    print(form.validate_on_submit())
-    print(form.is_submitted())
-    print(form.validate())
-    print("------------------sdfsdfsfs--------------")
+
+    
     if form.validate_on_submit():
         attempt_admin = Admin.query.filter_by(username=form.username.data).first()
-        print('-----------------kkkk--------------------')
+        
         if attempt_admin and attempt_admin.isvalid_password(
             attempt_password = form.password.data
             ):
-            print('kms')
+            
             login_user(attempt_admin)
             flash(f'Success! you are logged in as :{attempt_admin.username}', category='success')
             return redirect(url_for('home'))
         else:
-            print("ggggggggggggggg")
+            
             flash('Username and password are not match! Please try again', category='danger')
     return render_template("login.html", form=form)
     
