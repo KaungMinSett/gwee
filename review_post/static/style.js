752f88986c82cfd653df938423e39a87b6b15946
Your Name <you@example.com>
2022-10-07 14:01:44 +0630

    updated homepage content

--- review_post/static/style.js
+++ review_post/static/style.js
@@ -1,5 +1,5 @@
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+let backtoTop = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -11,14 +11,14 @@ function scrollFunction() {
     document.body.scrollTop > 20 ||
     document.documentElement.scrollTop > 20
   ) {
-    mybutton.style.display = "block";
+    backtoTop.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    backtoTop.style.display = "none";
   }
 }
 // When the user clicks on the button, scroll to the top of the document
-if(mybutton){
-    mybutton.addEventListener("click", backToTop);
+if(backtoTop){
+    backtoTop.addEventListener("click", backToTop);
 }
 
 function backToTop() {
